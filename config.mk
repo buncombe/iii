480d25c51e2dd2b7868ee43541a6d4b9105b59eb
je <je@ne.regrette.rien>
2011-02-05 00:01:38 +0100

    No change in functionality, but in doc and code.
    As a new mainline version has been rolled out, the changes that have been
    made to this git tree are committed before an update to the latest version will
    take place.
    
     * One new FAQ entry about reconnecting to servers and the wrapper program.
     * Fixed the layout of the LICENSE file.
     * Moved the EXECUTABLE variable from wrapper.c to config.mk.
     * Updated the man(1) page to contain the changes that have been made.
     * Restructured the code in wrapper.c.

--- config.mk
+++ config.mk
@@ -1,27 +1,32 @@
 # Customize to fit your system
 
+# If an environment variable is available, the affected setting below is either
+# skipped or (if the variable is holding flags) appended to the already defined
+# string.
+
 # paths
-PREFIX     ?= /usr/local
-BINDIR     ?= ${PREFIX}/bin
-MANDIR     ?= ${PREFIX}/share/man
-MAN1DIR    ?= ${MANDIR}/man1
-DOCDIR     ?= ${PREFIX}/share/doc/ii
+PREFIX		?= /usr/local
+BINDIR		?= ${PREFIX}/bin
+MANDIR		?= ${PREFIX}/share/man
+MAN1DIR		?= ${MANDIR}/man1
+DOCDIR		?= ${PREFIX}/share/doc/ii
 
 # Set the following to install to a different root
-DESTDIR    ?=
+DESTDIR		?=
 
-INCDIR     ?= ${PREFIX}/include
-LIBDIR     ?= ${PREFIX}/lib
-VERSION    ?= 1.4
+INCDIR		?= ${PREFIX}/include
+LIBDIR		?= ${PREFIX}/lib
+VERSION		?= 1.4
+EXECUTABLE	?= ii
 
 # includes and libs
-INCLUDES   += -I. -I${INCDIR} -I/usr/include
-LIBS       += -L${LIBDIR} -L/usr/lib -lc
+INCLUDES	+= -I. -I${INCDIR} -I/usr/include
+LIBS		+= -L${LIBDIR} -L/usr/lib -lc
 # uncomment and comment other variables for compiling on Solaris
-#LIBS       += -L${LIBDIR} -L/usr/lib -lc -lsocket -lnsl
-#CFLAGS     += -g ${INCLUDES} -DVERSION=\"${VERSION}\"
+#LIBS		+= -L${LIBDIR} -L/usr/lib -lc -lsocket -lnsl
+#CFLAGS		+= -g ${INCLUDES} -DVERSION=\"${VERSION}\" -DEXECUTABLE=\"${EXECUTABLE}\"
 
 # compiler
-CC         ?= cc
-CFLAGS     += -g -O0 -W -Wall ${INCLUDES} -DVERSION=\"${VERSION}\"
-LDFLAGS    += ${LIBS}
+CC		?= cc
+CFLAGS		+= -g -O0 -W -Wall ${INCLUDES} -DVERSION=\"${VERSION}\" -DEXECUTABLE=\"${EXECUTABLE}\"
+LDFLAGS		+= ${LIBS}
--- wrapper.c
+++ wrapper.c
@@ -8,14 +8,6 @@
 #include <time.h>
 #include <unistd.h>
 
-#define EXECUTABLE "ii"
-
-/*
- * Takes user specified hostnames and port numbers and randomly selects one pair[1]
- * before it passes them as arguments to EXECUTABLE via system(3). Whenever system(3)
- * dies, a new pair is selected[2].
- * [2] is done in the background while [1] is not.
- */
 int
 main(int argc, char **argv)
 {
@@ -24,51 +16,60 @@ main(int argc, char **argv)
 	size_t i, j, r, rv = 0, s;
 	pid_t pid, sid;
 
-	/* Validate the user input. */
-	if (uargc % 2 != 0 || uargc < 4)
-		err(1, "\"<ii args>\" <host> <port> <host1> <port1> ...\n");
+	if (uargc % 2 != 0 || uargc < 4) {
+		(void)fprintf(stderr, "usage: wrapper \"ii args\" host1 port1"
+		    " [host2 port2 ...]\n");
+		exit(1);
+	}
 
-	/*
-	 * sh(1) meta-characters: `<', `>', `|', `;', `(', `)', and `&'.
-	 * system(3) caveat:
-	 * http://www.openbsd.org/cgi-bin/man.cgi?query=system&apropos=0&sektion=0&manpath=OpenBSD+Current&arch=i386&format=html#CAVEATS
-	 */
 	for (i = 0; i < strlen(argv[1]); i++)
-		if (argv[1][i] == '<' || argv[1][i] == '>' || argv[1][i] == '|'
-		|| argv[1][i] == ';' || argv[1][i] == '(' || argv[1][i] == ')'
-		|| argv[1][i] == '&' || argv[1][i] == '"' || argv[1][i] == '\'')
-			err(1, "sh(1) meta-characters and quotation marks are"
-			" not allowed.\n");
+		if (argv[1][i] == '<' || argv[1][i] == '>'
+		    || argv[1][i] == '|' || argv[1][i] == ';'
+		    || argv[1][i] == '(' || argv[1][i] == ')'
+		    || argv[1][i] == '&' || argv[1][i] == '"'
+		    || argv[1][i] == '\'')
+			err(1, "sh(1) meta-characters and quotation marks are "
+			    "not allowed in the `ii args'.\n");
 
 	for (i = 2; i < uargc; i++)
 		if (i % 2 == 1) {
+			/*
+			 * Permit port numbers that are in the range 0 < p <
+			 * 65535.
+			 */
 			if (atoi(argv[i]) < 1 || atoi(argv[i]) > 65535)
-				err(1, "Port number `%lu' is not in range."
-				" (Counting from zero.)\n", (unsigned long)i / 2 - 1);
+				err(1, "Port number `%lu' is not in range.\n",
+				    (unsigned long)i / 2);
 			for (j = 0; j < strlen(argv[i]); j++)
-				if (argv[i][j] < 0x30 || argv[i][j] > 0x39) /* !0-9 */
+				if (argv[i][j] < 0x30 || argv[i][j] > 0x39)
 					err(1, "Character `%lu' in port `%lu'"
-					" is not allowed."
-					" (Counting from zero.)\n",
-					(unsigned long)j, (unsigned long)i / 2 - 1);
+					    " is not allowed.\n",
+					    (unsigned long)j + 1,
+					    (unsigned long)i / 2);
 		} else {
+			/*
+			 * Permitted characters in the hostname fields:
+			 * 	a-z	(0x61 < c < 0x7a)
+			 *	0-9	(0x30 < c < 0x39)
+			 *	A-Z	(0x41 < c < 0x5a)
+			 *	.-
+			 */
 			for (j = 0; j < strlen(argv[i]); j++)
-				if (((argv[i][j] < 0x61 || argv[i][j] > 0x7a) /* !a-z */
-				&& (argv[i][j] < 0x30 || argv[i][j] > 0x39) /* !0-9 */
-				&& (argv[i][j] < 0x41 || argv[i][j] > 0x5a)) /* !A-Z */
-				&& argv[i][j] != 0x2d && argv[i][j] != 0x2e) /* !. !- */
-					err(1, "Character `%lu' in hostname `%lu'"
-					" is not allowed."
-					" (Counting from zero.)\n",
-					(unsigned long)j, (unsigned long)i / 2 - 1);
+				if (((argv[i][j] < 0x61 || argv[i][j] > 0x7a)
+				    && (argv[i][j] < 0x30 || argv[i][j] > 0x39)
+				    && (argv[i][j] < 0x41 || argv[i][j] > 0x5a))
+				    && argv[i][j] != '.' && argv[i][j] != '-')
+					err(1, "Character `%lu' in hostname"
+					    " `%lu' is not allowed.\n",
+					    (unsigned long)j + 1,
+					    (unsigned long)i / 2);
 		}
 
-	/* Tell the user what we have got. */
+	printf("arguments: `%s'\n", argv[1]);
 	for (i = 2; i < uargc; i += 2)
 		printf("[%lu]: %s:%d\n", (unsigned long)(i - 1) / 2, argv[i],
-		atoi(argv[i + 1]));
+		    atoi(argv[i + 1]));
 
-	/* Lets continue in the background. */
 	pid = fork();
 	if (pid < 0) {
 		rv = 1;
@@ -103,7 +104,7 @@ main(int argc, char **argv)
 		r = random() * 1.0 / ( RAND_MAX + 1.0 ) * (i / 2 - 1);
 
 		s = strlen(argv[1]) + strlen(argv[r * 2 + 2]) +
-		strlen(argv[r * 2 + 3]) + 10 + sizeof(EXECUTABLE);
+		    strlen(argv[r * 2 + 3]) + 10 + sizeof(EXECUTABLE);
 
 		if ((init = calloc(s, 1)) == NULL) {
 			rv = 1;
