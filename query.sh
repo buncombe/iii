41c5f059d3cf2b67235b0f6039a05db335b3481b
je <je@ne.regrette.rien>
2011-02-05 01:42:57 +0100

    Query.sh, now not depending on specific host names

--- query.sh
+++ query.sh
@@ -5,25 +5,25 @@
 # Purpose: locate new queries for the ii irc client
 # ----------------------------------------------------
 
-IRCPATH=$HOME/irc
+IRCPATH=irc
 TMPFILE=$IRCPATH/queries.tmp
 
 if [ ! -f $TMPFILE ]; then
-    touch $TMPFILE
+	touch $TMPFILE
 fi
 
 echo "searching new query data"
 for i in `find $IRCPATH -newer $TMPFILE -name 'out'`
 do
-    grep -v '\-!\-' $i  > /dev/null 2>&1 # if file doesnt just contain server stuff
-    if [ $? -ne 1 ]; then
-        # strip server, nickserv and channel out files
-        echo $i | egrep -v -i "nickserv|#|$IRCPATH/(irc\.freenode\.net|irc\.oftc\.net)/out" > /dev/null 2>&1
-        if [ $? -ne 1 ]; then
-            printf "new data in: %s\n========================================================\n" "$i"
-            tail -5 $i
-        fi
-    fi
+	grep -v '\-!\-' $i  > /dev/null 2>&1 # if file doesnt just contain server stuff
+	if [ $? -ne 1 ]; then
+		# strip server, nickserv and channel out files
+		echo $i | egrep -v -i "(nick|chan)serv|#|$IRCPATH\/[a-z0-9\.\-]+\/out$" > /dev/null 2>&1
+		if [ $? -ne 1 ]; then
+			printf "new data in: %s\n========================================================\n" "$i"
+			tail -5 $i
+		fi
+	fi
 done
 
 touch $TMPFILE
