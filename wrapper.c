6c769045dd8bfdc8df8b12b623113c8e2690881c
je <je@ne.regrette.rien>
2011-02-12 23:39:23 +0100

    wrapper.c rewritten, support for exec on connect.

--- wrapper.c
+++ wrapper.c
@@ -1,94 +1,92 @@
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 #include <err.h>
+#include <errno.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 #include <unistd.h>
+
+#define LOCATE_HP(a, b) (((a) - (b)) / 2 + 1)
+
+static size_t	host_is_valid(char *);
+static size_t	is_sh_clear(char *);
+static int	port_is_valid(char *);
+static void	usage(void);
 
 int
 main(int argc, char **argv)
 {
 	const unsigned int uargc = (unsigned int)argc;
-	char *init = NULL;
-	size_t i, j, r, rv = 0, s;
-	pid_t pid, sid;
+	char *args[2] = {NULL, NULL}, *init = NULL;
+	size_t n, o, s;
+	int ch, rv;
+	pid_t pids[2], sid;
 
-	if (uargc % 2 != 0 || uargc < 4) {
-		(void)fprintf(stderr, "usage: wrapper \"ii args\" host1 port1"
-		    " [host2 port2 ...]\n");
-		exit(1);
-	}
-
-	for (i = 0; i < strlen(argv[1]); i++)
-		if (argv[1][i] == '<' || argv[1][i] == '>'
-		    || argv[1][i] == '|' || argv[1][i] == ';'
-		    || argv[1][i] == '(' || argv[1][i] == ')'
-		    || argv[1][i] == '&' || argv[1][i] == '"'
-		    || argv[1][i] == '\'')
-			err(1, "sh(1) meta-characters and quotation marks are "
-			    "not allowed in the `ii args'.\n");
-
-	for (i = 2; i < uargc; i++)
-		if (i % 2 == 1) {
-			/*
-			 * Permit port numbers that are in the range 0 < p <=
-			 * 65535.
-			 */
-			if (atoi(argv[i]) < 1 || atoi(argv[i]) > 65535)
-				err(1, "Port number `%lu' is not in range.\n",
-				    (unsigned long)i / 2);
-			for (j = 0; j < strlen(argv[i]); j++)
-				if (argv[i][j] < 0x30 || argv[i][j] > 0x39)
-					err(1, "Character `%lu' in port `%lu'"
-					    " is not allowed.\n",
-					    (unsigned long)j + 1,
-					    (unsigned long)i / 2);
-		} else {
-			/*
-			 * Permitted characters in the hostname fields:
-			 * 	a-z	(0x61 <= c <= 0x7a)
-			 *	0-9	(0x30 <= c <= 0x39)
-			 *	A-Z	(0x41 <= c <= 0x5a)
-			 *	.-
-			 */
-			for (j = 0; j < strlen(argv[i]); j++)
-				if (((argv[i][j] < 0x61 || argv[i][j] > 0x7a)
-				    && (argv[i][j] < 0x30 || argv[i][j] > 0x39)
-				    && (argv[i][j] < 0x41 || argv[i][j] > 0x5a))
-				    && argv[i][j] != '.' && argv[i][j] != '-')
-					err(1, "Character `%lu' in hostname"
-					    " `%lu' is not allowed.\n",
-					    (unsigned long)j + 1,
-					    (unsigned long)i / 2);
+	if (uargc < 3) usage();
+	while ((ch = getopt(argc, argv, "i:s:")) != -1)
+		switch (ch) {
+		case 'i':
+			if (is_sh_clear(optarg))
+				errx(1, "sh(1) meta-characters and quotation"
+				    " marks are not allowed in `ii arg'.");
+			args[0] = optarg;
+			break;
+		case 's':
+			if (is_sh_clear(optarg))
+				errx(1, "sh(1) meta-characters and quotation"
+				    " marks are not allowed in `sh arg'.");
+			args[1] = optarg;
+			break;
+		default:
+			usage();
+			/* NOTREACHED */
 		}
 
-	printf("arguments: `%s'\n", argv[1]);
-	for (i = 2; i < uargc; i += 2)
-		printf("[%lu]: %s:%d\n", (unsigned long)(i - 1) / 2, argv[i],
-		    atoi(argv[i + 1]));
+	for (o = 1; o < uargc - 1; o++)
+		if (argv[o][0] == '-' && argv[o][1] == '-')
+			break;
+	o++; /* argv[k] is now the first hostname, hopefully: */
+	if (o == uargc || (uargc - o) & 1) usage();
 
-	pid = fork();
-	if (pid < 0) {
-		rv = 1;
-		goto clean;
-	} else if (pid > 0)
-		goto clean;
+	for (n = o; n < uargc; n++) /* Validate the hosts and ports. */
+		if ((n - o) & 1) {
+			if ((rv = port_is_valid(argv[n])) < 0)
+				errx(1, "Port number `%lu' is not in range.",
+				    (unsigned long)LOCATE_HP(n, o));
+			else if (rv > 0)
+				errx(1, "Character `%lu' in port `%lu' is not"
+				    " allowed.", (unsigned long)rv,
+				    (unsigned long)LOCATE_HP(n, o));
+		} else
+			if ((rv = host_is_valid(argv[n])) != NULL)
+				errx(1, "Character `%lu' in hostname `%lu' is"
+				    " not allowed.", (unsigned long)rv,
+				    (unsigned long)LOCATE_HP(n, o));
+
+	if (args[0]) printf("ii arg: `%s'.\n", args[0]);
+	if (args[1]) printf("sh arg: `%s'.\n", args[1]);
+	for (n = o; n < uargc - 1; n += 2)
+		printf("Server[%lu]: %s:%d\n", (unsigned long)LOCATE_HP(n, o),
+		    argv[n], atoi(argv[n + 1]));
+
+	pids[0] = fork();
+	if (pids[0] < 0)
+		exit(1);
+	else if (pids[0] > 0)
+		exit(0);
 
 	umask(0);
 
 	sid = setsid();
-	if (sid < 0) {
-		rv = 1;
-		goto clean;
-	}
+	if (sid < 0)
+		_exit(1);
 
-	if ((chdir("/")) < 0) {
-		rv = 1;
-		goto clean;
-	}
+	if ((chdir("/")) < 0)
+		_exit(1);
 
 	close(STDIN_FILENO);
 	close(STDOUT_FILENO);
@@ -96,33 +94,102 @@ main(int argc, char **argv)
 
 	srandom(time(NULL));
 
-	while (1) {
-		/*
-		 * Calculate a random number in the range 0-(i/2 - 1). 
-		 * http://eternallyconfuzzled.com/arts/jsw_art_rand.aspx
-		 */
-		r = random() * 1.0 / ( RAND_MAX + 1.0 ) * (i / 2 - 1);
-
-		s = strlen(argv[1]) + strlen(argv[r * 2 + 2]) +
-		    strlen(argv[r * 2 + 3]) + 10 + sizeof(EXECUTABLE);
-
-		if ((init = calloc(s, 1)) == NULL) {
-			rv = 1;
-			goto clean;
+	for (;;) {
+		if (args[1] && pids[1] > 0) {
+			if (kill(pids[1], SIGKILL) && errno != ESRCH)
+				_exit(1);
+			waitpid(0, NULL, 0);
 		}
+
+		/* http://eternallyconfuzzled.com/arts/jsw_art_rand.aspx */
+		n = o + 1 + random() * 1.0 / ( RAND_MAX + 1.0 ) * (uargc - o - 1);
+		if ((n - o) & 1) n--;
+		s = strlen(args[0]) + strlen(argv[n]) + strlen(argv[n + 1]) + 10
+		    + sizeof(EXECUTABLE);
+
+		if ((init = calloc(s, 1)) == NULL)
+			_exit(1);
 		strlcpy(init, EXECUTABLE" ", s);
-		strlcat(init, argv[1], s);
+		strlcat(init, args[0], s);
 		strlcat(init, " -s ", s);
-		strlcat(init, argv[r * 2 + 2], s);
+		strlcat(init, argv[n], s);
 		strlcat(init, " -p ", s);
-		strlcat(init, argv[r * 2 + 3], s);
+		strlcat(init, argv[n + 1], s);
 
-		system(init);
-		free(init);
+		if (args[1]) {
+			pids[1] = fork();
+			if (pids[1] < 0) {
+				_exit(1);
+			} else if (pids[1] > 0) {
+				system(init);
+				free(init);
+				continue;
+			}
+
+			sleep(60);
+			system(args[1]);
+			_exit(0);
+		} else {
+			system(init);
+			free(init);
+		}
 	}
+}
 
-clean:
-	if (init != NULL)
-		free(init);
-	return rv;
+static size_t
+host_is_valid(char *h)
+{
+	size_t n;
+
+	/*
+	 * Permitted characters in a hostname:
+	 * 	a-z	(0x61 <= c <= 0x7a)
+	 *	0-9	(0x30 <= c <= 0x39)
+	 *	A-Z	(0x41 <= c <= 0x5a)
+	 *	.-
+	 */
+	for (n = 0; n < strlen(h); n++)
+		if (((h[n] < 0x61 || h[n] > 0x7a) && (h[n] < 0x30 ||
+		    h[n] > 0x39) && (h[n] < 0x41 || h[n] > 0x5a))
+		    && h[n] != '.' && h[n] != '-')
+			return n + 1;
+	return 0;
+}
+
+static size_t
+is_sh_clear(char *s)
+{
+	size_t n;
+
+	for (n = 0; n < strlen(s); n++)
+		if (s[n] == '<' || s[n] == '>' || s[n] == '|' || s[n] == ';' ||
+		    s[n] == '(' || s[n] == ')' || s[n] == '&' || s[n] == '"' ||
+		    s[n] == '\'')
+			if (n && s[n - 1] != '\\')
+				return n + 1;
+	return 0;
+}
+
+static int
+port_is_valid(char *p)
+{
+	size_t n;
+
+	if (atoi(p) < 1 || atoi(p) > 65535)
+		return -1;
+
+	for (n = 0; n < strlen(p); n++)
+		if (p[n] < 0x30 || p[n] > 0x39)
+			return n + 1;
+	return 0;
+}
+
+static void
+usage(void)
+{
+	extern char *__progname;
+
+	(void)fprintf(stderr, "usage: %s [-i ii arg] [-s sh arg] \\\n\t --"
+	    " host1 port1 [host2 port2 ...]\n", __progname);
+	exit(1);
 }
