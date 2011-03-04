a55338ad341945d2f96ba3f3b71f7542e4ef4c8c
buncombe <github@example.com>
2011-03-04 22:15:26 +0100

    Change the naming of variables in wrapper.c.

--- wrapper.c
+++ wrapper.c
@@ -15,73 +15,76 @@
 #include <string.h>
 #include <unistd.h>
 
-#define LOCATE_HP(a, b) (((a) - (b)) / 2 + 1)
+#define FIND_ELEM(a, b) (((a) - (b)) / 2 + 1)
 
-static size_t	host_is_valid(char *);
-static size_t	is_sh_clear(char *);
-static int	port_is_valid(char *);
+static size_t	valhost(char *);
+static int	valport(char *);
+static size_t	valsysarg(char *);
 static void	usage(void);
 
 int
 main(int argc, char **argv)
 {
-	const unsigned int uargc = (unsigned int)argc;
-	char *args[2] = {NULL, NULL}, *init = NULL;
-	size_t n, o, s;
-	int ch, rv;
-	pid_t pids[2], sid;
+	char *iiarg = NULL, *iicmd = NULL, *sharg = NULL;
+	size_t size;
+	int ch, i, j, rv;
+	pid_t iipid, shpid, sid;
 
-	if (uargc < 3) usage();
+	if (argc < 3)
+		usage();
 	while ((ch = getopt(argc, argv, "i:s:")) != -1)
 		switch (ch) {
 		case 'i':
-			if (is_sh_clear(optarg))
+			if (valsysarg(optarg))
 				errx(1, "sh(1) meta-characters and quotation"
 				    " marks are not allowed in `ii arg'.");
-			args[0] = optarg;
+			iiarg = optarg;
 			break;
 		case 's':
-			if (is_sh_clear(optarg))
+			if (valsysarg(optarg))
 				errx(1, "sh(1) meta-characters and quotation"
 				    " marks are not allowed in `sh arg'.");
-			args[1] = optarg;
+			sharg = optarg;
 			break;
 		default:
 			usage();
 			/* NOTREACHED */
 		}
 
-	for (o = 1; o < uargc - 2; o++)
-		if (!strcmp(argv[o], "--"))
+	for (i = 1; i < argc - 2; i++)
+		if (!strcmp(argv[i], "--"))
 			break;
-	o++; /* argv[o] is now the first hostname, hopefully: */
-	if ((uargc - o) & 1) usage();
+	i++; /* argv[i] is now the first hostname, hopefully: */
+	if ((argc - i) & 1)
+		usage();
 
-	for (n = o; n < uargc; n++) /* Validate the hosts and ports. */
-		if ((n - o) & 1) {
-			if ((rv = port_is_valid(argv[n])) < 0)
+	for (j = i; j < argc; j++) /* Validate the hosts and ports. */
+		if ((j - i) & 1) {
+			if ((rv = valport(argv[j])) < 0)
 				errx(1, "Port number `%lu' is not in range.",
-				    (unsigned long)LOCATE_HP(n, o));
+				    (unsigned long)FIND_ELEM(j, i));
 			else if (rv > 0)
 				errx(1, "Character `%lu' in port `%lu' is not"
 				    " allowed.", (unsigned long)rv,
-				    (unsigned long)LOCATE_HP(n, o));
+				    (unsigned long)FIND_ELEM(j, i));
 		} else
-			if ((rv = host_is_valid(argv[n])) != NULL)
+			if ((rv = valhost(argv[j])) != NULL)
 				errx(1, "Character `%lu' in hostname `%lu' is"
 				    " not allowed.", (unsigned long)rv,
-				    (unsigned long)LOCATE_HP(n, o));
+				    (unsigned long)FIND_ELEM(j, i));
 
-	if (args[0]) printf("ii arg: `%s'.\n", args[0]);
-	if (args[1]) printf("sh arg: `%s'.\n", args[1]);
-	for (n = o; n < uargc - 1; n += 2)
-		printf("Server[%lu]: %s:%d\n", (unsigned long)LOCATE_HP(n, o),
-		    argv[n], atoi(argv[n + 1]));
+	if (iiarg)
+		printf("ii arg: `%s'.\n", iiarg);
+	if (sharg)
+		printf("sh arg: `%s'.\n", sharg);
+	for (j = i; j < argc - 1; j += 2)
+		printf("Server[%lu]: %s:%d\n", (unsigned long)FIND_ELEM(j, i),
+		    argv[j], atoi(argv[j + 1]));
 
-	pids[0] = fork();
-	if (pids[0] < 0)
+	iipid = fork();
+	if (iipid < 0)
 		err(1, "Failed to create child process.");
-	else if (pids[0] > 0)
+	else if (iipid > 0)
 		exit(0);
 
 	umask(0);
@@ -93,12 +96,11 @@ main(int argc, char **argv)
 	close(STDIN_FILENO);
 	close(STDOUT_FILENO);
 	close(STDERR_FILENO);
-
 	srandom(time(NULL));
 
 	for (;;) {
-		if (args[1] && pids[1] > 0) {
-			if (kill(pids[1], SIGKILL) && errno != ESRCH)
+		if (sharg && shpid > 0) {
+			if (kill(shpid, SIGKILL) && errno != ESRCH)
 				_exit(1);
 			/* FIXME: see SA_NOCLDWAIT in sigaction(3). */
 			waitpid(0, NULL, 0);
@@ -107,44 +109,45 @@ main(int argc, char **argv)
 		sleep(LOOP_SLEEP);
 
 		/* http://eternallyconfuzzled.com/arts/jsw_art_rand.aspx */
-		n = o + 1 + random() * 1.0 / ( RAND_MAX + 1.0 ) * (uargc - o - 1);
-		if ((n - o) & 1) n--;
-		s = strlen(args[0]) + strlen(argv[n]) + strlen(argv[n + 1]) + 11
-		    + strlen(II_EXEC);
+		j = i + 1 + random() * 1.0 / ( RAND_MAX + 1.0 ) * (argc - i - 1);
+		if ((j - i) & 1)
+			j--;
+		size = strlen(iiarg) + strlen(argv[j]) + strlen(argv[j + 1]) +
+		    11 + strlen(II_EXEC);
 
-		if ((init = calloc(s, 1)) == NULL)
+		if ((iicmd = calloc(size, 1)) == NULL)
 			_exit(1);
-		strlcpy(init, II_EXEC" ", s);
-		strlcat(init, args[0], s);
-		strlcat(init, " -s ", s);
-		strlcat(init, argv[n], s);
-		strlcat(init, " -p ", s);
-		strlcat(init, argv[n + 1], s);
+		strlcpy(iicmd, II_EXEC" ", size);
+		strlcat(iicmd, iiarg, size);
+		strlcat(iicmd, " -s ", size);
+		strlcat(iicmd, argv[j], size);
+		strlcat(iicmd, " -p ", size);
+		strlcat(iicmd, argv[j + 1], size);
 
-		if (args[1]) {
-			pids[1] = fork();
-			if (pids[1] < 0) {
+		if (sharg) {
+			shpid = fork();
+			if (shpid < 0) {
 				_exit(1);
-			} else if (pids[1] > 0) {
-				system(init); /* FIXME: Check return value. */
-				free(init);
+			} else if (shpid > 0) {
+				system(iicmd); /* FIXME: Check return value. */
+				free(iicmd);
 				continue;
 			}
 
 			sleep(CHLD_SLEEP);
-			system(args[1]); /* FIXME: Check return value. */
+			system(sharg); /* FIXME: Check return value. */
 			_exit(0);
 		} else {
-			system(init);  /* FIXME: Check return value. */
-			free(init);
+			system(iicmd);  /* FIXME: Check return value. */
+			free(iicmd);
 		}
 	}
 }
 
 static size_t
-host_is_valid(char *h)
+valhost(char *h)
 {
-	size_t n;
+	size_t i;
 
 	/*
 	 * Permitted characters in a hostname:
@@ -154,39 +157,39 @@ host_is_valid(char *h)
 	 *	.-
 	 */
 	/* FIXME: Is too restrictive in its current shape. */
-	for (n = 0; n < strlen(h); n++)
-		if (((h[n] < 0x61 || h[n] > 0x7a) && (h[n] < 0x30 ||
-		    h[n] > 0x39) && (h[n] < 0x41 || h[n] > 0x5a))
-		    && h[n] != '.' && h[n] != '-')
-			return n + 1;
-	return 0;
-}
-
-static size_t
-is_sh_clear(char *s)
-{
-	size_t n;
-
-	for (n = 0; n < strlen(s); n++)
-		if (s[n] == '<' || s[n] == '>' || s[n] == '|' || s[n] == ';' ||
-		    s[n] == '(' || s[n] == ')' || s[n] == '&' || s[n] == '"' ||
-		    s[n] == '\'')
-			if (n && s[n - 1] != '\\')
-				return n + 1;
+	for (i = 0; i < strlen(h); i++)
+		if (((h[i] < 0x61 || h[i] > 0x7a) && (h[i] < 0x30 ||
+		    h[i] > 0x39) && (h[i] < 0x41 || h[i] > 0x5a))
+		    && h[i] != '.' && h[i] != '-')
+			return i + 1;
 	return 0;
 }
 
 static int
-port_is_valid(char *p)
+valport(char *p)
 {
-	size_t n;
+	size_t i;
 
 	if (atoi(p) < 1 || atoi(p) > 65535)
 		return -1;
 
-	for (n = 0; n < strlen(p); n++)
-		if (p[n] < 0x30 || p[n] > 0x39)
-			return n + 1;
+	for (i = 0; i < strlen(p); i++)
+		if (p[i] < 0x30 || p[i] > 0x39)
+			return i + 1;
+	return 0;
+}
+
+static size_t
+valsysarg(char *s)
+{
+	size_t i;
+
+	for (i = 0; i < strlen(s); i++)
+		if (s[i] == '<' || s[i] == '>' || s[i] == '|' || s[i] == ';' ||
+		    s[i] == '(' || s[i] == ')' || s[i] == '&' || s[i] == '"' ||
+		    s[i] == '\'')
+			if (i && s[i - 1] != '\\')
+				return i + 1;
 	return 0;
 }
 
